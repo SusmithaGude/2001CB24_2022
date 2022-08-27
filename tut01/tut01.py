90a0962d2bae105d2744c1f5c73ac2a2db922da8
Susmitha Gude <susmithaisi123@gmail.com>
2022-08-27 22:54:15 +0530

    updated code 1

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,14 @@
-def factorial(int x):
-    hello
-
-x=int(input("Enter the number whose factorial is to be found"))
-factorial(x)
+def factorial(x):
+    if x < 0:
+        print(" factorial doesn't exixts for negative numbers")
+        
+    elif x == 0 or x == 1:
+        return 1 
+    
+    else: 
+       # factorial = 1 
+        return x * factorial(x-1)
+        
+    
+x = int(input("Enter the number whose factorial is to be found"))
+print("factorial of ",x,"is ",factorial(x))
